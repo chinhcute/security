b14c810c9aace04b9ba29070b1e12ae94a75fc09
ADMIN <chinh20043008@gmail.com>
2023-06-07 18:02:53 +0700

    Commit and Push

--- src/main/java/mvc/confi/SecurityConfig.java
+++ src/main/java/mvc/confi/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login")
                 .permitAll()
                 .and()
+
                 .logout()
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("/")
